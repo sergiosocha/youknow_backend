007d25a4c8993633efe227b8c4142afabbf85370
sergi <sergio_socha@yahoo.com>
2025-12-13 15:19:00 -0500

    Create repository, Create Project

---
+++ src/main/java/com/yuno/youknow/YouknowApplication.java
@@ -0,0 +1,13 @@
+package com.yuno.youknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouknowApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yuno/youknow/db/orm/evento.java
@@ -0,0 +1,6 @@
+package com.yuno.youknow.db.orm;
+
+public class evento {
+
+
+}
